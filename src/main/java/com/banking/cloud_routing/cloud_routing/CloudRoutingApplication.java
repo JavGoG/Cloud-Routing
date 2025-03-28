24769c2b5124afaedd546b8e4b88bfc71935f3f8
JavGog <javierggjavier@gmail.com>
2025-03-28 18:13:37 +0100

    Initial commit

---
+++ src/main/java/com/banking/cloud_routing/cloud_routing/CloudRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.banking.cloud_routing.cloud_routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRoutingApplication.class, args);
+	}
+
+}
